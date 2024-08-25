913ebac24eaccb3cb99415b5907b57c6d1e219ec
Nahyan Quasem <nahyan.quasem+github@gmail.com>
2024-08-25 10:50:22 +0600

    Delegated all rps button events to a single listener

--- interface.js
+++ interface.js
@@ -1,6 +1,5 @@
 
 const playGameButton = document.querySelector('#playGameButton');
-
 const playerChoiceButton = document.querySelector('#playerChoice');
 
 
@@ -10,22 +9,8 @@ playGameButton.addEventListener('click', ()=>{
 
 playerChoiceButton.addEventListener('click', (e) => {
 
-    let buttonId = e.target.id;
+    let playerChoice = e.target.textContent;
+    console.log(playerChoice);
     
-    switch (buttonId){
-        case 'rockButton':
-            console.log('The rock button was pressed');
-            break;
-        case 'paperButton':
-            console.log('The paper button was pressed');
-            break;
-        case 'scissorButton':
-            console.log('The scissor button was pressed');
-            break;
-        default:
-            console.log('Invalid Input');
-
-    }
-
 });
 
