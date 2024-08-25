f0b2f89339bcd6bb2689967ebf6411433edb78f3
Nahyan Quasem <nahyan.quasem+github@gmail.com>
2024-08-25 09:03:11 +0600

    Adds button functionality to RPS buttons.
    Stub testing RPS buttons.

--- interface.js
+++ interface.js
@@ -1,7 +1,31 @@
 
 const playGameButton = document.querySelector('#playGameButton');
 
+const playerChoiceButton = document.querySelector('#playerChoice');
+
 
 playGameButton.addEventListener('click', ()=>{
     playGame();
 });
+
+playerChoiceButton.addEventListener('click', (e) => {
+
+    let buttonId = e.target.id;
+    
+    switch (buttonId){
+        case 'rockButton':
+            console.log('The rock button was pressed');
+            break;
+        case 'paperButton':
+            console.log('The paper button was pressed');
+            break;
+        case 'scissorButton':
+            console.log('The scissor button was pressed');
+            break;
+        default:
+            console.log('Invalid Input');
+
+    }
+
+});
+
