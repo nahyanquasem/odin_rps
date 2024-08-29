c715549aa0833af8175148473e4cf09fa53c96fb
Nahyan Quasem <nahyan.quasem+github@gmail.com>
2024-08-29 09:23:26 +0600

    Fixes ghost inputs from playerChoice div
    Assigns event listeners to playerChoice node list.
    Make efficient use of node list and for each.

--- interface.js
+++ interface.js
@@ -1,11 +1,11 @@
 
 // const playGameButton = document.querySelector('#playGameButton');
-const playerChoiceButton = document.querySelector('#playerChoice');
+const playerChoiceButton = document.querySelectorAll('#playerChoiceButton');
 const resetButton = document.querySelector('#resetButton');
 
 const playerScoreValue = document.querySelector('#playerScore');
 const computerScoreValue = document.querySelector('#computerScore');
-const roundScoreValue = document.querySelector('#roundInfo')
+const roundScoreValue = document.querySelector('#roundInfo');
 
 
 // playGameButton.addEventListener('click', ()=>{
@@ -14,16 +14,28 @@ const roundScoreValue = document.querySelector('#roundInfo')
 
 resetButton.addEventListener('click', () =>{
     resetGame();
-    showScores(humanScore, computerScore)
-});
-
-playerChoiceButton.addEventListener('click', (e) => {
-
-    let playerChoice = e.target.textContent;
-    playRound(playerChoice);
     showScores(humanScore, computerScore);
-    
 });
+
+playerChoiceButton.forEach((button)=>{
+
+    button.addEventListener('click', (e) => {
+
+        let playerChoice = e.target.textContent;
+        playRound(playerChoice);
+        showScores(humanScore, computerScore);
+
+    });
+
+});
+
+// playerChoiceButton.addEventListener('click', (e) => {
+
+//     let playerChoice = e.target.textContent;
+//     playRound(playerChoice);
+//     showScores(humanScore, computerScore);
+    
+// });
 
 function showScores(humanScore, computerScore){
     
